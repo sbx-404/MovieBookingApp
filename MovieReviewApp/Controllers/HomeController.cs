2ae0282de737090d2814e8a8f02d07f9b4e65edb
Sbx-404 <sbsumit404@gmail.com>
2025-05-02 16:29:45 +0530

    make corousel in home page

--- MovieReviewApp/Controllers/HomeController.cs
+++ MovieReviewApp/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MovieReviewApp.Models;
+using MovieReviewApp.Services;
 
 namespace MovieReviewApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly string _apiKey;
+        private readonly apiService _apiService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IConfiguration configuration, apiService apiService)
         {
             _logger = logger;
+            _apiService = apiService;
+            _apiKey = configuration["API_KEY:Key"];
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult>  Index()
         {
-            return View();
+            var MovieURL = $"https://api.themoviedb.org/3/movie/upcoming?api_key={_apiKey}&language=en-US&page=1";
+            MovieResponse MovieData = await _apiService.MovieApi<MovieResponse>(MovieURL);
+
+            if (MovieData.Results == null)
+            {
+                return NotFound("Movie not found");
+            }
+
+            return View(MovieData.Results ?? new List<Movie>());
         }
 
         public IActionResult Privacy()
